6eff05f0028ed7931cb7218d6600d23af04a32ff
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2020-09-14 17:38:04 +0200

    feat: adds addLiItem function

--- js/app.js
+++ js/app.js
@@ -17,10 +17,12 @@
  * Define Global Variables
  *
  */
-
+let documentFragment = new DocumentFragment();
 const navbar = document.querySelector('.page__header .navbar__menu');
 const ul = document.querySelector('.page__header #navbar__list');
-const sections = document.querySelectorAll('main section');
+//! modify the node tree to array for no problems later
+const sections = Array.from(document.querySelectorAll('main section'));
+console.log('## TCL ## >>: sections', sections);
 
 /**
  * End Global Variables
@@ -36,6 +38,20 @@ const sections = document.querySelectorAll('main section');
 
 // build the nav
 
+function addLiItem() {
+  for (let i = 0; i < sections.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = sections[i].dataset.nav;
+    console.log(li.textContent);
+    li.classList.add('menu__link');
+
+    documentFragment.appendChild(li);
+  }
+  ul.appendChild(documentFragment);
+  //   console.log('## TCL ## >>: addLiItem -> documentFragment', documentFragment);
+}
+
+addLiItem();
 // Add class 'active' to section when near top of viewport
 
 // Scroll to anchor ID using scrollTO event
